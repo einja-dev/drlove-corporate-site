3e4c747d5c9e5c948216bb6b1e2dd9eeabf0f08b
Kazuki Watanabe <k_watanabe@yumemi.co.jp>
2025-07-25 21:14:13 +0900

    refactor: ページ構造の整理
    - src/app/a/page.tsxの修正
    - src/app/b/page.tsxの削除
    - src/app/page.tsxの修正
    
    変更内容：
    - 不要なページファイルの削除
    - メインページの構造改善

--- src/app/a/page.tsx
+++ src/app/a/page.tsx
@@ -2,8 +2,8 @@ import CompanyLogoSection from '@/app/components/CompanyLogoSection';
 import CompanySection from '@/app/components/top/CompanySection';
 import ContentLinkSection from '@/app/components/top/ContentLinkSection';
 import GallerySection from '@/app/components/top/GallerySection';
-import HeroSection from '@/app/components/top/Hero/HeroSection';
-import SplashWrapper from '@/app/components/top/Hero/SplashWrapper';
+import HeroSection2 from '@/app/components/top/Hero/HeroSection2';
+import SplashWrapper2 from '@/app/components/top/Hero/SplashWrapper2';
 import MemberSection from '@/app/components/top/MemberSection/MemberSection';
 import MessageSection from '@/app/components/top/MessageSection';
 import NewsSection from '@/app/components/top/NewsSection';
@@ -13,9 +13,9 @@ import RecruitSection from '@/app/components/top/RecruitSection';
 export default function APage() {
   return (
     <>
-      <SplashWrapper>
-        <HeroSection />
-      </SplashWrapper>
+      <SplashWrapper2>
+        <HeroSection2 />
+      </SplashWrapper2>
       <MessageSection />
       <ContentLinkSection />
       <OurServiceSection />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,20 @@ import CompanyLogoSection from '@/app/components/CompanyLogoSection';
 import CompanySection from '@/app/components/top/CompanySection';
 import ContentLinkSection from '@/app/components/top/ContentLinkSection';
 import GallerySection from '@/app/components/top/GallerySection';
-import HeroSection2 from '@/app/components/top/Hero/HeroSection2';
-import SplashWrapper2 from '@/app/components/top/Hero/SplashWrapper2';
+import HeroSection from '@/app/components/top/Hero/HeroSection';
+import SplashWrapper from '@/app/components/top/Hero/SplashWrapper';
 import MemberSection from '@/app/components/top/MemberSection/MemberSection';
 import MessageSection from '@/app/components/top/MessageSection';
 import NewsSection from '@/app/components/top/NewsSection';
 import OurServiceSection from '@/app/components/top/OurServiceSection';
 import RecruitSection from '@/app/components/top/RecruitSection';
 
-export default function Home() {
+export default function Page() {
   return (
     <>
-      <SplashWrapper2>
-        <HeroSection2 />
-      </SplashWrapper2>
+      <SplashWrapper>
+        <HeroSection />
+      </SplashWrapper>
       <MessageSection />
       <ContentLinkSection />
       <OurServiceSection />
